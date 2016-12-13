5458be618fedb298f87fe535cc9c1b1d8cb4ec6f
Jonathan Sanders <jps458@nyu.edu>
2016-12-13 15:21:24 +0000

    noticed that I need static categories and levels

--- muse/views.py
+++ muse/views.py
@@ -5,59 +5,78 @@ from . import app
 import copy
 import ast
 
+api_key = 'afb4ed7652b99475b548e55ddbca70bcb72575fa881bc2c0a652e2ec0150356b'
+
 @app.route("/")
 @app.route("/jobs", methods=["GET"])
 def jobs_get():
-    r = requests.get('https://api-v2.themuse.com/jobs')
-
-    print(r.status_code)
+    initial_call = requests.get('https://api-v2.themuse.com/jobs?api_key=' + api_key, {'page' : 1})
+    #print(r.status_code)
     #print(r.headers['content-type'])
     #print(r.json, "json")
     #print(r.text, "text")
-    
-    payload = {'page' : 1}
-    z = requests.get('https://api-v2.themuse.com/jobs', params=payload)
-    print(z)
+    parsed_json = json.loads(initial_call.text)
+    page = parsed_json['page_count']
+    payload = {'page' : page}
+    updated_request = requests.get('https://api-v2.themuse.com/jobs?api_key=' +
+    api_key, params=payload)
+    print(updated_request)
     #print(z.text)
-    print(z.url)
-    parsed_json = json.loads(z.text)
-    '''for _ in parsed_json['results']:
-        name = _['name']
-        company = _['company']
-        print(company['name'], ":", name)'''
+    print(updated_request.url)
+    print(parsed_json['page_count'], "PAGECOUNT")
     
     #company_title = [(_['name'] , _['company']['name']) for _ in parsed_json['results']]
-    all_category = []
-    for _ in parsed_json['results']:
-        categories = _['categories']
-        for category in categories:
-            #print(location['name'])
-            all_category.append(category['name'])
-    #print(company_title)
-    #locations = [company['locations'] for company in parsed_json['results']]
-    z = []
+    all_category =  ['Account Management',
+                    'Business & Strategy',
+                    'Creative & Design',
+                    'Customer Service',
+                    'Data Science',
+                    'Editorial',
+                    'Education',
+                    'Engineering',
+                    'Finance',
+                    'Fundraising & Development',
+                    'Healthcare & Medicine',
+                    'HR & Recruiting',
+                    'Legal',
+                    'Marketing & PR',
+                    'Operations',
+                    'Project & Product Management',
+                    'Retail',
+                    'Sales',
+                    'Social Media & Community']
+    
+    all_levels = ['Internship',
+                    'Entry Level',
+                    'Mid Level',
+                    'Senior Level']
+    all_locations = []
     companies = []
     for _ in parsed_json['results']:
         locations = _['locations']
         company = _['company']
         for location in locations:
             #print(location['name'])
-            z.append(location['name'])
+            all_locations.append(location['name'])
         for comp in [company]:
             companies.append(comp['name'])
 
-    return render_template("jobs.html", all_category=sorted(all_category), locations=sorted(z), companies=sorted(companies))
+    return render_template('jobs.html', 
+                            all_category=all_category, 
+                            all_locations=sorted(all_locations), 
+                            companies=sorted(companies), 
+                            all_levels=all_levels)
 
 @app.route("/jobs", methods=["POST"])
 def jobs_post():
-    choose_locations = request.form.getlist("location")
-    payload = {"page" : 1}
-    categories = request.form.getlist("category")
-    companies = request.form.getlist("company")
-    payload["locations"] = choose_locations
-    payload["categories"] = categories
-    payload["company"] = companies
-    z = requests.get('https://api-v2.themuse.com/jobs', params=payload)
+    choose_locations = request.form.getlist('location')
+    payload = {'page' : 1}
+    categories = request.form.getlist('category')
+    companies = request.form.getlist('company')
+    payload['locations'] = choose_locations
+    payload['categories'] = categories
+    payload['company'] = companies
+    z = requests.get('https://api-v2.themuse.com/jobs?api_key=' + api_key, params=payload)
     print(payload, "PRINTING PAYLOAD1")
     print(z.url, "URL1")
     return redirect(url_for('test_get', payload=payload))
@@ -69,7 +88,7 @@ def test_get(payload):
     #payload_string = json.loads(payload)
     #payload_dump = json.dumps(payload_string)
     payload = ast.literal_eval(payload)
-    z = requests.get('https://api-v2.themuse.com/jobs', params=payload)
+    z = requests.get('https://api-v2.themuse.com/jobs?api_key=' + api_key, params=payload)
     print(z.url, "URL")
     
     '''parsed_json = json.loads(z.url)
