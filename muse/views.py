a138ee250c37072fc77bba1acc44d3477be9a0af
Jonathan Sanders <jps458@nyu.edu>
2016-12-13 23:37:22 +0000

    procfile and style

--- muse/views.py
+++ muse/views.py
@@ -13,18 +13,10 @@ api_key = 'afb4ed7652b99475b548e55ddbca70bcb72575fa881bc2c0a652e2ec0150356b'
 @app.route("/jobcriteria", methods=["GET"])
 def job_criteria_get():
     initial_call = requests.get('https://api-v2.themuse.com/jobs', {'page' : 1})
-    #print(r.status_code)
-    #print(r.headers['content-type'])
-    #print(r.json, "json")
-    #print(r.text, "text")
     parsed_json = json.loads(initial_call.text)
     page = parsed_json['page_count']
     payload = {'page' : page}
     updated_request = requests.get('https://api-v2.themuse.com/jobs', params=payload)
-    print(updated_request)
-    #print(z.text)
-    print(updated_request.url)
-    print(parsed_json['page_count'], "PAGECOUNT")
     all_category =  ['Account Management',
                     'Business & Strategy',
                     'Creative & Design',
@@ -44,13 +36,11 @@ def job_criteria_get():
                     'Retail',
                     'Sales',
                     'Social Media & Community']
-    
     all_levels = ['Internship',
                     'Entry Level',
                     'Mid Level',
                     'Senior Level']
-                    
-    all_locations = []
+    '''all_locations = []
     companies = []
     for _ in parsed_json['results']:
         locations = _['locations']
@@ -59,36 +49,23 @@ def job_criteria_get():
             all_locations.append(location['name'])
         for comp in [company]:
             companies.append(comp['name'])
-    job_listings = "not yet"
+    '''
     
     return render_template('jobcriteria.html', 
                             all_category=all_category, 
-                            companies=sorted(companies), 
-                            all_levels=all_levels,
-                            job_listings=job_listings)
+                            #companies=sorted(companies), 
+                            all_levels=all_levels)
 
 @app.route("/jobcriteria", methods=["POST"])
 def job_criteria_post():
     full_fuzzy=[]
-    
     payload = {'page' : 1}
     choose_locations = request.form['location']
     if len(choose_locations) != 0:
         full_fuzzy = locationsearch.locations(choose_locations)
-        chosen_location = locationsearch.locations(choose_locations)[0][0]
-        payload['location'] = chosen_location
-    print(2)
-    
-    categories = request.form.getlist('category')
-    levels = request.form.getlist('level')
-    
-    payload['category'] = categories
-    #payload['company'] = companies
-    payload['level'] = levels
-    z = requests.get('https://api-v2.themuse.com/jobs', params=payload)
-    
-    print(payload, "PRINTING PAYLOAD1")
-    print(z.url, "URL1")
+        payload['location'] = locationsearch.locations(choose_locations)[0][0]
+    payload['category'] = request.form.getlist('category')
+    payload['level'] = request.form.getlist('level')
     return redirect(url_for('job_results_get', payload=payload, full_fuzzy=full_fuzzy, page=1))
     
 @app.route("/jobresults/<payload>/<full_fuzzy>/<page>", methods=["GET"])
@@ -100,16 +77,13 @@ def job_results_get(payload, full_fuzzy, page):
         flash("Displaying results for " + full_fuzzy2[0][0] + 
                 ".  Please change your search if you meant " + 
                 full_fuzzy2[1][0], "success")
-    z = requests.get('https://api-v2.themuse.com/jobs', params=payload2)
-    print(z.url, "URL2")
-    parsed_json = json.loads(z.text)
+    call = requests.get('https://api-v2.themuse.com/jobs', params=payload2)
+    parsed_json = json.loads(call.text)
     last_page = parsed_json['page_count']
     all_all = {}
     for _ in parsed_json['results']:
         all_all[_['name']] = [_['company']['name'],_['id']]
-    
-    print(all_all, "ALL ALL")
-
+    #print(all_all, "ALL ALL")
     return render_template("jobresults.html", all_all=all_all,
                             page=int(page), 
                             full_fuzzy=full_fuzzy2,
@@ -117,14 +91,12 @@ def job_results_get(payload, full_fuzzy, page):
     
 @app.route("/jobresults", methods=["POST"])
 def job_results_post(payload):
-    #print(z.url)
     return render_template("jobresults.html")
     
 @app.route("/listing/<id>", methods = ["GET"])
 def listing_get(id):
-    a = requests.get('https://api-v2.themuse.com/jobs/' + id)
-    parsed_json = json.loads(a.text)
-    #print("PARSED:", parsed_json, "PARDDED")
+    single = requests.get('https://api-v2.themuse.com/jobs/' + id)
+    parsed_json = json.loads(single.text)
     print(parsed_json["refs"]["landing_page"], "PRINTED")
     return redirect(parsed_json["refs"]["landing_page"])
     
