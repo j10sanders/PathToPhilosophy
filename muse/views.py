a60caee01ff2e34ce2e9096ca0ea71f4b4991c8d
Jonathan Sanders <jps458@nyu.edu>
2016-12-14 19:10:08 +0000

    removed plurals, put companies in separate file

--- muse/views.py
+++ muse/views.py
@@ -5,10 +5,28 @@ from . import app
 import copy
 import ast
 import fuzzywuzzy
-from . import locationsearch
+from . import locationsearch, companylist
 
 api_key = 'afb4ed7652b99475b548e55ddbca70bcb72575fa881bc2c0a652e2ec0150356b'
+companies = set()
 
+
+# Will need to do this asyncronously.  Takes about 10 seconds.
+def get_companies(page=0, last_page=100):
+    call_comps = requests.get('https://api-v2.themuse.com/companies?api_key=' +
+    api_key + '&page=' + str(page))
+    parsed_json = json.loads(call_comps.text)
+    if page == 0:
+        last_page = parsed_json['page_count']
+    while page < last_page:
+        for _ in parsed_json['results']:
+            companies.add(_['name'])
+        page+=1
+        return get_companies(page, last_page)
+    return sorted(companies)
+    
+
+# Choose criteria for job search.
 @app.route("/", methods=["GET"])
 @app.route("/jobcriteria", methods=["GET"])
 def job_criteria_get():
@@ -35,42 +53,41 @@ def job_criteria_get():
                 'Entry Level',
                 'Mid Level',
                 'Senior Level']
-    '''all_locations = []
-    companies = []
-    for _ in parsed_json['results']:
-        locations = _['locations']
-        company = _['company']
-        for location in locations:
-            all_locations.append(location['name'])
-        for comp in [company]:
-            companies.append(comp['name'])
-    '''
-    
     return render_template('jobcriteria.html', 
                             all_category=all_category, 
-                            all_levels=all_levels)
+                            all_levels=all_levels,
+                            all_companies=companylist.companies)
 
+# Add criteria to api call
 @app.route("/", methods=["POST"])
 @app.route("/jobcriteria", methods=["POST"])
 def job_criteria_post():
     full_fuzzy=[]
     payload = {'page' : 1}
     choose_locations = request.form['location']
+    # Use fuzzy search to find closest two location matches
     if len(choose_locations) != 0:
         full_fuzzy = locationsearch.locations(choose_locations)
         payload['location'] = locationsearch.locations(choose_locations)[0][0]
     payload['category'] = request.form.getlist('category')
     payload['level'] = request.form.getlist('level')
-    return redirect(url_for('job_results_get', payload=payload, full_fuzzy=full_fuzzy, page=1))
-    
+    payload['company'] = request.form.getlist('company')
+    return redirect(url_for('job_results_get',
+                            payload=payload,
+                            full_fuzzy=full_fuzzy,
+                            page=1))
+
+# Return jobs    
 @app.route("/jobresults/<payload>/<full_fuzzy>/<page>", methods=["GET"])
 def job_results_get(payload, full_fuzzy, page):
     payload2 = ast.literal_eval(payload)
     full_fuzzy2 = ast.literal_eval(full_fuzzy)
     payload2['page'] = page
+    
+    # Fuzzy search returns top 2 matches, in order.  use first but send both on 
+    # initial request. If the user selects the second one, just send that one.
     first_location, other_location = [], []
     if len(full_fuzzy2) > 0:
-        print(full_fuzzy2, len(full_fuzzy2))
         if len(full_fuzzy2) == 1:
             other_location = []
             first_location = full_fuzzy2
@@ -78,16 +95,24 @@ def job_results_get(payload, full_fuzzy, page):
         else:
             other_location = dict([full_fuzzy2[1]])
             first_location = full_fuzzy2[0][0]
-    call = requests.get('https://api-v2.themuse.com/jobs?api_key=' + api_key, params=payload2)
+    call = requests.get('https://api-v2.themuse.com/jobs?api_key=' +
+                        api_key, params=payload2)
+    print(call.url)
     parsed_json = json.loads(call.text)
     last_page = parsed_json['page_count']
-    all_all = {}
+    
+    # Send template a dictionary with {Job Title : [company name, job id]}
+    jobs_w_companies = {}
     for _ in parsed_json['results']:
-        all_all[_['name']] = [_['company']['name'],_['id']]
-    return render_template("jobresults.html", all_all=all_all,
+        jobs_w_companies[_['name']] = [_['company']['name'],_['id']]
+    return render_template("jobresults.html",
+                            jobs_w_companies=jobs_w_companies,
                             page=int(page), 
                             full_fuzzy=full_fuzzy2,
-                            payload=payload2, last_page=int(last_page), other_location=other_location, first_location=first_location)
+                            payload=payload2,
+                            last_page=int(last_page),
+                            other_location=other_location,
+                            first_location=first_location)
     
 @app.route("/jobresults", methods=["POST"])
 def job_results_post(payload):
@@ -130,7 +155,6 @@ def company_criteria_get():
 
     return render_template('companycriteria.html', 
                             industries=industries, 
-                            #companies=sorted(companies), 
                             sizes=sizes)
 
 @app.route("/", methods=["POST"])
@@ -144,5 +168,8 @@ def company_criteria_post():
         payload['location'] = locationsearch.locations(choose_locations)[0][0]
     payload['category'] = request.form.getlist('category')
     payload['level'] = request.form.getlist('level')
-    return redirect(url_for('company_results_get', payload=payload, full_fuzzy=full_fuzzy, page=1))
+    return redirect(url_for('company_results_get',
+                            payload=payload, 
+                            full_fuzzy=full_fuzzy,
+                            page=1))
     
